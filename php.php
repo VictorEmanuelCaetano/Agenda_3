79de5732bc0289ce7259cde555b17a9157161f20
VictorEmanuelCaetano <139991133+VictorEmanuelCaetano@users.noreply.github.com>
2026-08-18 19:00:06 -0300

    bugfix

--- php.php
+++ php.php
@@ -13,11 +13,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mensagem = "Olá $nome, sua compra de R$ $valorCompra foi realizada com cartão de crédito. Não há desconto. Valor total: R$".$total;
     } elseif ($formaPagamento == "boleto") {
         $desconto = $valorCompra * 0.08; // corrigido: 8% para boleto
-        $total = number_format(($valorCompra * $desconto) /100, 2, ',', '.');
+        $total = number_format(($valorCompra - $desconto), 2, ',', '.');
         $mensagem = "Olá $nome, sua compra de R$ $valorCompra foi realizada com boleto. Seu desconto é de R$ $desconto. Valor total: R$".$total;
     } elseif ($formaPagamento == "deposito") {
         $desconto = $valorCompra * 0.1; // corrigido: 10% para depósito
-        $total = number_format(($valorCompra * $desconto) /100, 2, ',', '.');
+        $total = number_format(($valorCompra - $desconto), 2, ',', '.');
         $mensagem = "Olá $nome, sua compra de R$ $valorCompra foi realizada com depósito. Seu desconto é de R$ $desconto. Valor total: R$".$total;
     } else {
         $mensagem = "Forma de pagamento inválida.";
